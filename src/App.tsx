2eb16b50670e68aad70d343bd10b9cc85dacf3e6
yoshiki-0633 <132421616+yoshiki-0633@users.noreply.github.com>
2023-05-22 19:54:02 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
+
+export default function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>こんにちは</p>
+    </ChakraProvider>
+  );
+}
